f75c053754ec466c8e6a129611848e9bdf19140a
shina <rasheedshinaopeyemi@gmail.com>
2020-06-30 20:49:20 +0100

    added the featured component

---
+++ src/components/nav/index.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { Link } from "gatsby"
+import { window } from "browser-monads"
+import logo from "../../images/compass-logo.svg"
+import "./nav.css"
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="nav__items">
+        <a href="/ " className="nav__item--left">
+          <img
+            src={logo}
+            alt="Traveller Pack Logo"
+            className="nav__item--logo "
+          />
+        </a>
+        <Link
+          to="/contact"
+          className={
+            window.location.href.indexOf("contact") > 0
+              ? "nav__item--link active"
+              : "nav__item--link"
+          }
+        >
+          Contact
+        </Link>
+
+        <Link
+          to="/blog"
+          className={
+            window.location.href.indexOf("blog") > 0 ||
+            window.location.href.indexOf("category") > 0
+              ? "nav__item--link active"
+              : "nav__item--link"
+          }
+        >
+          Blog
+        </Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
